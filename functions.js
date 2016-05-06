4c127172030420647ac0f895c6cf02a8d2d84c3c
Ubuntu <ubuntu@ip-172-31-43-205.us-west-2.compute.internal>
2016-05-06 01:31:51 +0000

    url validation and date time stamp added

---
+++ functions.js
@@ -0,0 +1,41 @@
+
+
+module.exports = {
+	getDateTime: function () {
+		var date = new Date();
+
+		var hour = date.getHours();
+		hour = (hour < 10 ? "0" : "") + hour;
+
+		var min  = date.getMinutes();
+		min = (min < 10 ? "0" : "") + min;
+
+		var sec  = date.getSeconds();
+		sec = (sec < 10 ? "0" : "") + sec;
+
+		var year = date.getFullYear();
+
+		var month = date.getMonth() + 1;
+		month = (month < 10 ? "0" : "") + month;
+
+		var day  = date.getDate();
+		day = (day < 10 ? "0" : "") + day;
+
+		return month + "/" + day + "/" + year + "  " + hour + ":" + min;	
+	},
+	// http://stackoverflow.com/questions/1701898/how-to-detect-whether-a-string-is-in-url-format-using-javascript
+	validateUrl: function(url) {
+		var strRegex = "^((https|http|ftp|rtsp|mms)?://)"
+			+ "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" //ftp的user@
+			+ "(([0-9]{1,3}\.){3}[0-9]{1,3}" // IP形式的URL- 199.194.52.184
+			+ "|" // 允许IP和DOMAIN（域名）
+			+ "([0-9a-z_!~*'()-]+\.)*" // 域名- www.
+			+ "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\." // 二级域名
+			+ "[a-z]{2,6})" // first level domain- .com or .museum
+			+ "(:[0-9]{1,4})?" // 端口- :80
+			+ "((/?)|" // a slash isn't required if there is no file name
+			+ "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";
+		var re=new RegExp(strRegex);
+		return re.test(url);
+	}
+};
